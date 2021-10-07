183e657c41793d4e1aa51d8ffa8fee959f7d7f4b
Carlo Castro <ccastro_200000000215@uic.edu.ph>
2021-10-07 15:03:39 +0800

    changes

--- dvd_server/rental/dbinsert_rental.js
+++ dvd_server/rental/dbinsert_rental.js
@@ -1,13 +1,13 @@
-const pool = require("./db");
+const pool = require("../db");
 
 pool.query(
   "INSERT INTO public.rental(rental_date, invertory_id, costumer_id, return_date, staff_id, last_update) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *",
-  ["2005-05-24 22:53:30"],
+  ["2005-05-24 21:53:30"],
   ["21"],
   ["21"],
-  ["2005-05-24 22:53:30"],
+  ["2005-05-24 21:53:30"],
   ["21"],
-  ["2005-05-24 22:53:30"],
+  ["2005-05-24 21:53:30"],
   (err, res) => {
     if (err) {
       console.log(err.stack);
