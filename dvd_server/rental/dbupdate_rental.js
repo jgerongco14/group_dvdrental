1bf10594ae7cb5b594ce323fc36c0d10e838b180
Carlo Castro <ccastro_200000000215@uic.edu.ph>
2021-10-07 19:28:49 +0800

    finish

---
+++ dvd_server/rental/dbupdate_rental.js
@@ -0,0 +1,11 @@
+const pool = require("./db");
+
+pool.query("UPDATE public.rental SET customer_id = $1 WHERE rental_date = 2005-05-24 22:53:30", ['120'], (err,res)=>{
+    if(err){
+        console.log(err.stack);
+    }
+    else{
+        console.log(res.rows);
+    }
+});
+pool.end();
